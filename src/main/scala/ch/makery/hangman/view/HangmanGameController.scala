5cd66269c95b5cac72afe0652bb8d737e6b21ef9
Eugene <qihao.eugenewong@gmail.com>
2024-08-21 22:31:26 +0800

    Wrote some part for HangmanGameController, added "drawHangman" function, some important thing like wrongCounter and chances, and the word. Eyes cannot open liao good night Dr Chin

--- src/main/scala/ch/makery/hangman/view/HangmanGameController.scala
+++ src/main/scala/ch/makery/hangman/view/HangmanGameController.scala
@@ -1,5 +1,7 @@
 package ch.makery.hangman.view
 
+import ch.makery.hangman.model.Word
+
 import scalafx.scene.control.{Alert, Label, TableColumn, TableView, TextField}
 import scalafxml.core.macros.sfxml
 import scalafx.beans.property.StringProperty
@@ -16,7 +18,35 @@ class HangmanGameController(
                              private val hangmanField : TextField
                            ) {
 
-  val chances = 6
+  //Initializing the game things
+  val chances: Int = 6
+  val wrongs: Int = 0
+  var currentWord: Word = _
+  //This one is for hiding the word, when the alphabet is not revealed
+  var hiddenWord: StringProperty = new StringProperty("")
+  val alphabetButtons = List("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+
+  wordLabel.text <== hiddenWord
+
+  def handleGameStart(actionEvent: ActionEvent) = {
+    //Get word from the database
+    val words = Word.getAllWords
+
+
+  }
+
+  def drawHangman(wrongCounter: Int): String = {
+    wrongCounter match {
+      case 0 => "  +---+\n  |   |\n      |\n      |\n      |\n      |\n========="
+      case 1 => "  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n========="
+      case 2 => "  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n========="
+      case 3 => "  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n========="
+      case 4 => "  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n========="
+      case 5 => "  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n========="
+      case 6 => "  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n========="
+    }
+  }
+
 }
 
 //
