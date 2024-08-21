b90252383b57e5adf6bc91bc27c2087be0df6dbb
Eugene <qihao.eugenewong@gmail.com>
2024-08-21 20:49:38 +0800

    Changed root file and added some functionality for database (from address app)

--- src/main/scala/ch/makery/hangman/model/Word.scala
+++ src/main/scala/ch/makery/hangman/model/Word.scala
@@ -8,14 +8,56 @@ import scala.util.{Failure, Success, Try}
 
 class Word(val wordS: String) extends Database {
   def this() = this("")
+
   var word = new StringProperty(wordS)
   var firstHint = new StringProperty("some hint")
   var secondHint = new StringProperty("some hint")
 
-  def save(): Unit = {
-
+  def save(): Try[Int] = {
+    if (!(isExist)) {
+      Try(DB autoCommit { implicit session =>
+        sql"""
+             insert into word (word, firstHint, secondHint) values
+             (${word.value}, ${firstHint.value}, ${secondHint.value})
+           """.update.apply()
+      })
+    } else {
+      Try(DB autoCommit { implicit session =>
+        sql"""
+             update word
+             set
+              word = ${word.value},
+              firstHint = ${firstHint.value},
+              secondHint = ${secondHint.value}
+           """.update.apply()
+      })
+    }
   }
+
+  def delete(): Try[Int] = {
+    if (isExist) {
+      Try(DB autoCommit { implicit session =>
+        sql"""
+             delete from word where
+             word = ${word.value}
+          """.update.apply()
+      })
+    } else
+      throw new Exception("Word doesn't exist")}
+
+    def isExist : Boolean = {
+      DB readOnly { implicit session =>
+        sql"""
+            select * from word where
+            word = ${word.value}
+         """.map(rs => rs.string("word")).single.apply()
+      } match {
+        case Some(x) => true
+        case None => false
+      }
+    }
 }
+
 
 object Word extends Database {
   def apply (
